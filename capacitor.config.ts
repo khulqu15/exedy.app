61f30a05eb0e49a0a18a2733572e82689a5df0e8
khulqu15 <khusnul.ninno15@gmail.com>
2022-08-12 20:29:30 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'exedy',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
